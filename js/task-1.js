082e6acf6a45c7e916cc84d88949ffe898251b26
Stanislav <opelkia@ukr.net>
2023-10-13 10:33:12 +0300

    start

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+'use strict';
+let quantity = 5;
+let pricePerDroid = 10;
+function makeTransaction( quantity, pricePerDroid ) {
+    console.log(`"Addition result equals" ${quantity + pricePerDroid}`);
+}
+
+
